a5a386b0852299473bb78d86a63966b660e63aef
Tõnu Mikk <tonumikk@yahoo.com>
2023-05-09 11:29:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let password1El = document.getElementById("password1")
+let password2El = document.getElementById("password2")
+
+function pickRandomIndex(){
+    return Math.floor(Math.random() * characters.length)
+}
+
+function generatePasswords(){
+    let password1 = ""
+    let password2 = ""
+    for (let i = 0; i < 15; i++ ){
+        password1 += characters[pickRandomIndex()]
+        password2 += characters[pickRandomIndex()]
+    }
+    password1El.textContent = password1
+    password2El.textContent = password2
+}
+
